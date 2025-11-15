51856b62db136da8ec5e62756e761bb803c57662
Renato Zannon <renato@rrsz.com.br>
2025-11-14 22:46:21 -0300

    Added Nix flake support

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "env-control Rust CLI flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forEachSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "env-control";
+            version = self.shortRev or "0.0.0-dev";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+        });
+
+      devShells = forEachSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              clippy
+              rustfmt
+              rustc
+            ];
+          };
+        });
+    };
+}
